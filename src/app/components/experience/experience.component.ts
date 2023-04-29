eb7c6e3dd3886c4890e550a80e4d1ba440d1bc4e
Paweł <pawel@MacBook-Air-Pawe.local>
2023-04-29 10:31:49 +0200

    add style, logo and experience component

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Experience } from './experience';
 
 @Component({
   selector: 'app-experience',
@@ -6,6 +7,48 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./experience.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class ExperienceComponent {
+  experiences:Experience[] = [
+    {
+      "companyName": 'Netguru',
+      "during": 'mar 2023',
+      'description': 'Rozwój oraz utrzymanie strony www.netguru.com - layoff',
+      'role': 'Web Developer (Growth Team)',
+      'logo': '../../../assets/img/netguru_logo.jpeg'
+    },
+    {
+      "companyName": 'Autenti.com',
+      "during": 'wrz 2022 - mar 2023 mies',
+      'description': 'Rozwój istniejących oraz tworzenie nowych stron Autenti <br> Technologie: HubSpot, HTML, CSS, ES6+',
+      'role': 'Frontend Developer',
+      'logo': '../../../assets/img/autenti_logo.jpeg'
+    },
+    {
+      "companyName": 'Salesmanago',
+      "during": 'mar 2021 - wrz 2022',
+      'description': `
+      Utrzymanie i rozwój aplikacji SaaS
+      - Tworzenie nowych funkcjonalności z wykorzystaniem Angular,
+      - Pokrycie testami nowego oraz istniejącego kodu,
+      -Rozwój oraz poprawa istniejącego kodu m.in AngularJS, jquery, JSP,
+      -Czynny udział w code review,
+      - kodowanie formularzy / maili / Landing Page / skryptow dedykowanych,
+      - organizacja pracy zespołu,
+      - raportowanie wyników zespołu,
+      - reagowanie na bieżące potrzeby zespołu,
+      - sprawy organizacyjne związane z pracą zespołu,
+      `,
+      'role': 'Frontend',
+      'logo': '../../../assets/img/salesmanago_logo.jpeg'
+    },
+    {
+      "companyName": 'Junior frontend developer',
+      "during": 'gru 2020 - lut 2021',
+      'description': 'Rozwój istniejących oraz tworzenie nowych stron Autenti <br> Technologie: HubSpot, HTML, CSS, ES6+',
+      'role': 'Frontend Developer',
+      'logo': '../../../assets/img/veventy_logo.jpeg'
+    },
+  ]
 
 }
---
+++ src/app/components/experience/experience.ts
@@ -0,0 +1,7 @@
+export interface Experience {
+    companyName:string;
+    during:string;
+    description:string;
+    role:string;
+    logo:string;
+}
