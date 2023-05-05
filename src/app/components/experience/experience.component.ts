1e32a5a6cf0e4a2cb399d187c32687a3763e7bf9
Paweł <pawel@MacBook-Air-Pawe.local>
2023-05-05 12:22:16 +0200

    change language

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -13,28 +13,28 @@ export class ExperienceComponent {
     {
       "companyName": 'Netguru',
       "during": 'mar 2023',
-      'description': 'W Netguru byłem odpowiedzialny za zakodowanie strony internetowej na platformie HubSpot. Do budowy strony wykorzystano HTML, CSS, JavaScript oraz HubL.',
+      'description': "At Netguru, I was responsible for coding a website on the HubSpot platform. To build the site, HTML, CSS, JavaScript, and HubL were used.",
       'role': 'Web Developer (Growth Team)',
       'logo': '../../../assets/img/netguru_logo.jpeg'
     },
     {
       "companyName": 'Autenti.com',
       "during": 'wrz 2022 - mar 2023 mies',
-      'description': 'W ramach pracy w Autenti byłem odpowiedzialny za stworzenie strony internetowej na platformie HubSpot. Wiązało się to z wykorzystaniem HTML, CSS, JavaScript oraz HubL do budowy strony.',
+      'description': "As part of my work at Autenti, I was responsible for creating a website on the HubSpot platform. This involved using HTML, CSS, JavaScript, and HubL to build the site.",
       'role': 'Frontend Developer',
       'logo': '../../../assets/img/autenti_logo.jpeg'
     },
     {
       "companyName": 'Salesmanago',
       "during": 'mar 2021 - wrz 2022',
-      'description': "W SalesManago byłem odpowiedzialny za utrzymaniem i rozwój aplikacji SaaS SalesManago, tworzenie nowych funkcjonalności z wykorzystaniem Angular, pokrycie testami nowego i istniejącego kodu, rozwój i poprawa istniejącego kodu m.in. Angular, AngularJS, jQuery, JSP, aktywny udział w code review, kodowanie formularzy / maili / Landing Page / skryptów dedykowanych oraz jako Team Leader organizacja pracy zespołu, raportowanie wyników zespołu, reagowanie na bieżące potrzeby zespołu oraz sprawy organizacyjne związane z pracą zespołu.",
+      'description': "At SalesManago, I was responsible for maintaining and developing the SalesManago SaaS application, creating new functionalities using Angular, providing test coverage for new and existing code, developing and improving existing code including Angular, AngularJS, jQuery, JSP, actively participating in code reviews, coding forms/emails/Landing Pages/dedicated scripts, and as a Team Leader, organizing the team's work, reporting on team results, responding to the team's current needs, and handling organizational matters related to team work.",
       'role': 'Junior Frontend Developer/Frontend Developer',
       'logo': '../../../assets/img/salesmanago_logo.jpeg'
     },
     {
       "companyName": 'Veventy',
       "during": 'gru 2020 - lut 2021',
-      'description': 'Pracując w Veventy byłem odpowiedzialny za wykonanie strony internetowej streamspace.pl',
+      'description': "While working at Veventy, I was responsible for creating the streamspace.pl website.",
       'role': 'Junior frontend developer',
       'logo': '../../../assets/img/veventy_logo.jpeg'
     },
